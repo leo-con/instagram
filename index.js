9bdaea60ef8968300aa61630eb9676d10a654080
Leonel Contreras <leonel.contreras@inin.com>
2022-03-18 16:56:02 -0400

    .

--- index.js
+++ index.js
@@ -7,20 +7,32 @@ function getParameterByName(parameter) {
 }
 
 function getToken() {
+  const vcode = getParameterByName("code");
+
+  postData = {
+    client_id: "1288968731596687",
+    client_secret: "5391836b14e83ca53d5470a2c521778b",
+    grant_type: "authorization_code",
+    redirect_uri: "https://leo-con.github.io/instagram/index.html",
+    code: vcode,
+  };
+
   const v_url = "https://api.instagram.com/oauth/access_token";
   $.ajax({
     type: "POST",
     url: v_url,
     data: postData,
-    success: test,
+    success: successfn,
     error: function (e) {
       console.log(e);
+      document.getElementById("error").value = JSON.stringify(e);
     },
   });
 }
 
-function test(data) {
+function successfn(data) {
   console.log(data);
+  document.getElementById("success").value = JSON.stringify(data);
 }
 
 function showMedia(data) {
@@ -34,13 +46,4 @@ function showMedia(data) {
   console.log(data);
 }
 
-const vcode = getParameterByName("code");
-
-postData = {
-  client_id: "1288968731596687",
-  client_secret: "5391836b14e83ca53d5470a2c521778b",
-  grant_type: "authorization_code",
-  redirect_uri: "https://leo-con.github.io/instagram/index.html",
-  code: vcode,
-};
 getToken();
